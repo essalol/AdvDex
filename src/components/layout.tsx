f57c679dfd946c65ac25bf1da8b469b0749deb87
Farman Shaik <shkfrmn786@gmail.com>
2024-11-29 02:27:12 +0530

    add footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./navbar";
+import Footer from "./footer";
 
 export default function Layout() {
 
   return (
-    <div className="flex flex-col min-h-screen pb-24">
-      <Navbar />
-      <div className="flex-1">
-        <Outlet />
+    <>
+      <div className="flex flex-col min-h-screen pb-24">
+        <Navbar />
+        <div className="flex-1">
+          <Outlet />
+        </div>
+        {/* <Web3Modal /> */}
       </div>
-      {/* <Web3Modal /> */}
-    </div>
+      <Footer />
+    </>
   );
 }
